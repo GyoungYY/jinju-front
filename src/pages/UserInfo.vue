c900b02997c63f7e0b08fb09c9e65088ffd120a7
yangguang <yangguang@bilibili.com>
2018-01-13 12:43:37 +0800

    头像初步

--- src/pages/UserInfo.vue
+++ src/pages/UserInfo.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="user-container">
         <el-form label-width="100px" class="info-form">
+            <el-upload
+                class="avatar-uploader"
+                action="http://upload.qiniup.com"
+                :show-file-list="false"
+                :data="postData"
+                :on-success="handleAvatarSuccess"
+                :before-upload="beforeAvatarUpload">
+                <img v-if="imageUrl" :src="imageUrl" class="avatar">
+                <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
             <el-form-item>
                 <h4 style="color: #f90;margin: 0;">{{joinTime}}入网，在网时长{{diffDay || '0小时'}}</h4>
             </el-form-item>
@@ -34,6 +44,11 @@
                 infoDetail: {},
                 joinTime: '',
                 diffDay: '',
+
+                imageUrl: '',
+                postData:{
+                    token: 'aI9GR6VbK_5gu3kwDj-eTFny-1Hi4sucXf5mQkeg:28x3c--LOhWr6E_R2qP9k_njr9I=:eyJzY29wZSI6Imppbmp1bWFvIiwiZGVhZGxpbmUiOjE1MTU4NTc3NTJ9'
+                }
             }
         },
         mounted() {
@@ -68,6 +83,22 @@
                     this.$message.error(reason);
                 });
             },
+
+            handleAvatarSuccess(res, file) {
+                this.imageUrl = URL.createObjectURL(file.raw);
+            },
+            beforeAvatarUpload(file) {
+                const isJPG = file.type === 'image/jpeg';
+                const isLt2M = file.size / 1024 / 1024 < 2;
+
+//                if (!isJPG) {
+//                    this.$message.error('上传头像图片只能是 JPG 格式!');
+//                }
+                if (!isLt2M) {
+                    this.$message.error('上传头像图片大小不能超过 2MB!');
+                }
+                return isJPG && isLt2M;
+            }
         }
     }
 </script>
@@ -89,5 +120,29 @@
     .info-form {
         max-width: 600px;
         margin: auto;
+    }
+
+    .avatar-uploader .el-upload {
+        border: 1px dashed #d9d9d9;
+        border-radius: 6px;
+        cursor: pointer;
+        position: relative;
+        overflow: hidden;
+    }
+    .avatar-uploader .el-upload:hover {
+        border-color: #409EFF;
+    }
+    .avatar-uploader-icon {
+        font-size: 28px;
+        color: #8c939d;
+        width: 178px;
+        height: 178px;
+        line-height: 178px;
+        text-align: center;
+    }
+    .avatar {
+        width: 178px;
+        height: 178px;
+        display: block;
     }
 </style>
