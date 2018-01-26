29a29c58ae892c368e60b2405a4f3486d32341ca
yangguang <yangguang@bilibili.com>
2018-01-27 01:36:22 +0800

    websocket初步接入

--- src/pages/chat/ChatRoom.vue
+++ src/pages/chat/ChatRoom.vue
@@ -6,7 +6,7 @@
                 <div class="user-list" id="userList">
                     <div style="padding-top: 10px;color: #999;">在线列表</div>
                     <div v-for="item in userList" class="user-row">
-                        <img src="../../../static/img/photo0.jpeg" alt="" class="user-photo">
+                        <img :src="item.photoUrl" alt="" class="user-photo" @click="gotoUserPage(item.userId)">
                         <span>{{item.username}}</span>
                     </div>
                 </div>
@@ -15,14 +15,14 @@
                 <div class="message-notice" id="messageList">
 
                     <div v-for="item in messageList">
-                        <div :class="{'self-message' : false}">
-                            <div :class="{'self-username' : false}">
-                                <img src="../../../static/img/photo0.jpeg" alt="" class="message-photo">
-                                <span>aguang</span>
+                        <div :class="{'self-message' : item.userId == userInfo.userId}">
+                            <div :class="{'self-username' : item.userId == userInfo.userId}">
+                                <img :src="item.photoUrl" alt="" class="message-photo" @click="gotoUserPage(item.userId)">
+                                <span>{{item.username}}</span>
                             </div>
                             <div style="clear: both;"></div>
-                            <span class="message-content">
-                            这是一条消息这是一条消息这是一条消息这是一条消息这是一条消息这是一条消息
+                            <span class="message-content" :class="{'self-content': item.userId == userInfo.userId}">
+                                {{item.message}}
                             </span>
                         </div>
                         <div style="clear: both;"></div>
@@ -39,268 +39,212 @@
 </template>
 
 <script>
-    $.fn.scrollUnique = function () {
-        return $(this).each(function () {
-            var eventType = 'mousewheel';
-            if (document.mozHidden !== undefined) {
-                eventType = 'DOMMouseScroll';
-            }
-            $(this).on(eventType, function (event) {
-                // 一些数据
-                var scrollTop = this.scrollTop,
-                    scrollHeight = this.scrollHeight,
-                    height = this.clientHeight;
-
-                var delta = (event.originalEvent.wheelDelta) ? event.originalEvent.wheelDelta : -(event.originalEvent.detail || 0);
-
-                if ((delta > 0 && scrollTop <= delta) || (delta < 0 && scrollHeight - height - scrollTop <= -1 * delta)) {
-                    // IE浏览器下滚动会跨越边界直接影响父级滚动，因此，临界时候手动边界滚动定位
-                    this.scrollTop = delta > 0 ? 0 : scrollHeight;
-                    // 向上滚 || 向下滚
-                    event.preventDefault();
-                }
-            });
-        });
-    };
-
-
-    export default {
-        data() {
-            return {
-                sendText: '',
-                userList: [
-                    {
-                        photoUrl: '../../../static/img/photo0.jpeg',
-                        username: 'aguang',
-                    },
-                    {
-                        photoUrl: '../../../static/img/photo0.jpeg',
-                        username: 'aguang',
-                    },
-                    {
-                        photoUrl: '../../../static/img/photo0.jpeg',
-                        username: 'aguang',
-                    },
-                    {
-                        photoUrl: '../../../static/img/photo0.jpeg',
-                        username: 'aguang',
-                    },
-                    {
-                        photoUrl: '../../../static/img/photo0.jpeg',
-                        username: 'aguang',
-                    },
-                    {
-                        photoUrl: '../../../static/img/photo0.jpeg',
-                        username: 'aguang',
-                    },
-                    {
-                        photoUrl: '../../../static/img/photo0.jpeg',
-                        username: 'aguang',
-                    },
-                    {
-                        photoUrl: '../../../static/img/photo0.jpeg',
-                        username: 'aguang',
-                    },
-                    {
-                        photoUrl: '../../../static/img/photo0.jpeg',
-                        username: 'aguang',
-                    },
-                    {
-                        photoUrl: '../../../static/img/photo0.jpeg',
-                        username: 'aguang',
-                    },
-                    {
-                        photoUrl: '../../../static/img/photo0.jpeg',
-                        username: 'aguang',
-                    },
-                    {
-                        photoUrl: '../../../static/img/photo0.jpeg',
-                        username: 'aguang',
-                    },
-                    {
-                        photoUrl: '../../../static/img/photo0.jpeg',
-                        username: 'aguang',
-                    },
-                    {
-                        photoUrl: '../../../static/img/photo0.jpeg',
-                        username: 'aguang',
-                    },
-                    {
-                        photoUrl: '../../../static/img/photo0.jpeg',
-                        username: 'aguang',
-                    },
-                ],
-
-                messageList: [
-                    {
-                        photoUrl: '../../../static/img/photo0.jpeg',
-                        username: 'aguang',
-                        content: '这是一条消息内容'
-                    },
-                    {
-                        photoUrl: '../../../static/img/photo0.jpeg',
-                        username: 'aguang',
-                        content: '这是一条消息内容'
-                    },
-                    {
-                        photoUrl: '../../../static/img/photo0.jpeg',
-                        username: 'aguang',
-                        content: '这是一条消息内容'
-                    },
-                    {
-                        photoUrl: '../../../static/img/photo0.jpeg',
-                        username: 'aguang',
-                        content: '这是一条消息内容'
-                    },
-                    {
-                        photoUrl: '../../../static/img/photo0.jpeg',
-                        username: 'aguang',
-                        content: '这是一条消息内容'
-                    },
-                    {
-                        photoUrl: '../../../static/img/photo0.jpeg',
-                        username: 'aguang',
-                        content: '这是一条消息内容'
-                    },
-                ],
-
-                websock: null,
-            }
-        },
-
-        mounted() {
-            $('#userList').scrollUnique();
-            $('#messageList').scrollUnique();
-            this.initWebSocket();
-        },
-
-        methods: {
-
-            //初始化
-            initWebSocket(){
-                const wsUrl = "";
-                this.websock = new WebSocket(wsUrl);
-                this.websock.onmessage = this.getMessageList();  //指定收到服务器数据后的回调函数
-                this.websock.onerror = this.errorShow();  //用于指定报错时的回调函数
-            },
-
-            //获取消息列表
-            getMessageList(e) {
-                let readData = JSON.parse(e.data);
-            },
-
-            //发送消息
-            sendMessage() {
-                this.$message.success('发送成功');
-                this.websock.send(this.sendText);
-            },
-
-            //错误回调函数
-            errorShow(){
-                this.$message.success('webSocket有点问题，您喝杯茶再来试一下～');
-            },
-        }
+$.fn.scrollUnique = function() {
+  return $(this).each(function() {
+    var eventType = "mousewheel";
+    if (document.mozHidden !== undefined) {
+      eventType = "DOMMouseScroll";
     }
+    $(this).on(eventType, function(event) {
+      // 一些数据
+      var scrollTop = this.scrollTop,
+        scrollHeight = this.scrollHeight,
+        height = this.clientHeight;
+
+      var delta = event.originalEvent.wheelDelta
+        ? event.originalEvent.wheelDelta
+        : -(event.originalEvent.detail || 0);
+
+      if (
+        (delta > 0 && scrollTop <= delta) ||
+        (delta < 0 && scrollHeight - height - scrollTop <= -1 * delta)
+      ) {
+        // IE浏览器下滚动会跨越边界直接影响父级滚动，因此，临界时候手动边界滚动定位
+        this.scrollTop = delta > 0 ? 0 : scrollHeight;
+        // 向上滚 || 向下滚
+        event.preventDefault();
+      }
+    });
+  });
+};
+
+export default {
+  data() {
+    return {
+      userInfo: JSON.parse(sessionStorage.getItem("userInfo")),
+      sendText: "",
+      userList: [],
+      messageList: [],
+      websock: null
+    };
+  },
+
+  mounted() {
+    $("#userList").scrollUnique();
+    $("#messageList").scrollUnique();
+    this.initWebSocket();
+  },
+
+  methods: {
+    //初始化
+    initWebSocket() {
+      const wsUrl = "ws://localhost:8888/chatsocket/" + this.userInfo.userId;
+      this.websocket = new WebSocket(wsUrl);
+      //指定收到服务器数据后的回调函数
+      this.websocket.onmessage = event => {
+        this.getMessageList(event);
+      };
+
+      //用于指定报错时的回调函数
+      this.websocket.onerror = () => {
+        this.errorShow();
+      };
+    },
+
+    //获取消息列表
+    getMessageList(event) {
+      let result = JSON.parse(event.data);
+      console.log(result);
+      this.messageList.push(result);
+      if (result.type == "2") {
+        this.userList = result.userList;
+      }
+    },
+
+    //发送消息
+    sendMessage() {
+      if (!this.sendText) {
+        this.$message.error("请输入要发送的内容");
+        return;
+      }
+      this.$message.success("发送成功");
+      this.websocket.send(this.sendText);
+      this.sendText = "";
+    },
+
+    //错误回调函数
+    errorShow() {
+      this.$message.success("webSocket有点问题，您喝杯茶再来试一下～");
+    },
+
+    //关闭连接
+    closeConnection() {
+      this.websocket.close();
+    },
+
+    //进入用户个人主页
+    gotoUserPage(id) {
+      this.$router.push({ path: "/index/userPage/" + id });
+    }
+  }
+};
 </script>
 
 <style scoped>
-    .chat-container {
-        width: 880px;
-        margin: 30px auto;
-        padding: 20px;
-        background-color: #fff;
-        position: relative;
-    }
+.chat-container {
+  width: 880px;
+  margin: 30px auto;
+  padding: 20px;
+  background-color: #fff;
+  position: relative;
+}
 
-    .user-part {
-        position: absolute;
-        top: 20px;
-        left: 20px;
-        width: 200px;
-        padding-right: 20px;
-    }
+.user-part {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  width: 200px;
+  padding-right: 20px;
+}
 
-    .user-row {
-        margin: 15px 0;
-    }
+.user-row {
+  margin: 15px 0;
+}
 
-    .cur-num {
-        font-size: 16px;
-        border-bottom: 1px solid #ddd;
-        line-height: 50px;
-    }
+.cur-num {
+  font-size: 16px;
+  border-bottom: 1px solid #ddd;
+  line-height: 50px;
+}
 
-    .user-list {
-        height: 545px;
-        overflow-y: auto;
-    }
+.user-list {
+  height: 545px;
+  overflow-y: auto;
+}
 
-    .user-photo {
-        width: 30px;
-        height: 30px;
-        border-radius: 15px;
-        margin-right: 4px;
-    }
+.user-photo {
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  margin-right: 4px;
+  cursor: pointer;
+}
 
-    .message-part {
-        margin-left: 200px;
-    }
+.message-part {
+  margin-left: 200px;
+}
 
-    .message-notice {
-        height: 400px;
-        border: 1px solid #ddd;
-        padding: 10px;
-        margin-bottom: 15px;
-        overflow-y: auto;
-    }
+.message-notice {
+  height: 400px;
+  border: 1px solid #ddd;
+  padding: 10px;
+  margin-bottom: 15px;
+  overflow-y: auto;
+  border-radius: 4px;
+}
 
-    .message-photo {
-        width: 20px;
-        height: 20px;
-        border-radius: 10px;
-        margin-right: 4px;
-    }
+.message-photo {
+  width: 20px;
+  height: 20px;
+  border-radius: 10px;
+  margin-right: 4px;
+  cursor: pointer;
+}
 
-    .message-content {
-        display: inline-block;
-        padding: 8px 10px;
-        border: 1px solid #ddd;
-        border-radius: 5px;
-        margin: 10px 10px 15px 10px;
-        max-width: 300px;
-    }
+.message-content {
+  display: inline-block;
+  padding: 8px 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  margin: 10px 10px 15px 10px;
+  max-width: 300px;
+}
 
-    .send-div {
-    }
+.send-div {
+}
 
-    /* 设置滚动条的样式 */
-    ::-webkit-scrollbar {
-        width: 3px;
-    }
+/* 设置滚动条的样式 */
+::-webkit-scrollbar {
+  width: 3px;
+}
 
-    /* 滚动槽 */
-    ::-webkit-scrollbar-track {
-        -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
-        border-radius: 10px;
-    }
+/* 滚动槽 */
+::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  border-radius: 10px;
+}
 
-    /* 滚动条滑块 */
-    ::-webkit-scrollbar-thumb {
-        border-radius: 10px;
-        background: rgba(0, 0, 0, 0.1);
-        -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.2);
-    }
+/* 滚动条滑块 */
+::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  background: rgba(0, 0, 0, 0.1);
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.2);
+}
 
-    ::-webkit-scrollbar-thumb:window-inactive {
-        background: rgba(255, 0, 0, 0.4);
-    }
+::-webkit-scrollbar-thumb:window-inactive {
+  background: rgba(0, 0, 0, 0.2);
+}
 
-    .self-message{
-        float: right;
-    }
+.self-message {
+  float: right;
+}
 
-    .self-username{
-        float:right;
-        padding-right: 10px;
-    }
+.self-username {
+  float: right;
+  padding-right: 10px;
+}
+
+.self-content {
+  background-color: lawngreen;
+}
 </style>
