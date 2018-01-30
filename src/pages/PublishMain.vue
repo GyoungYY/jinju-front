6a361dd4dcf5381226d38a7a56f5b5906b002e37
yangguang <yangguang@bilibili.com>
2018-01-30 19:13:22 +0800

    富文本中图片上传处理

--- src/pages/PublishMain.vue
+++ src/pages/PublishMain.vue
@@ -49,21 +49,32 @@
                             :before-upload="beforeAvatarUpload">
                             <img v-if="article.imageUrl" :src="article.imageUrl" class="avatar">
                             <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-                        </el-upload>      
+                        </el-upload>
                     </el-form-item>
                 </el-form>
+
+                <el-upload
+                    class="quill-uploader"
+                    action="http://upload-z0.qiniup.com"
+                    :show-file-list="false"
+                    :data="postData"
+                    :on-success="uploadSuccess"
+                    :before-upload="beforeAvatarUpload"
+                    style="display: none;">
+                </el-upload>
+
                 <quill-editor v-model="article.content"
-                      ref="myQuillEditor"
-                      :options="options">
+                              ref="myQuillEditor"
+                              :options="options">
                 </quill-editor>
 
                 <el-row style="margin: 10px 0;float:right;">
-                        <el-button type="primary" @click="submitArticle('article')">立即创建</el-button>
-                        <el-button @click="resetArticle()">重置</el-button>
+                    <el-button type="primary" @click="submitArticle('article')">立即创建</el-button>
+                    <el-button @click="resetArticle()">重置</el-button>
                 </el-row>
                 <div style="clear:both;"></div>
 
-                <div class="ql-container ql-snow">
+                <div class="ql-container ql-snow" style="margin-bottom: 100px;">
                     <div class="ql-editor">
                         <div v-html="article.content" style=""></div>
                     </div>
@@ -75,186 +86,243 @@
 </template>
 
 <script>
-import JinjuInterface from "@/interface/JinjuInterface";
-import QiniuInterface from "@/interface/QiniuInterface";
-import MeiwenInterface from "@/interface/MeiwenInterface";
+    import JinjuInterface from "@/interface/JinjuInterface";
+    import QiniuInterface from "@/interface/QiniuInterface";
+    import MeiwenInterface from "@/interface/MeiwenInterface";
 
-import { quillEditor } from "vue-quill-editor";
-import "quill/dist/quill.core.css";
-import "quill/dist/quill.snow.css";
-import "quill/dist/quill.bubble.css";
+    import {quillEditor} from "vue-quill-editor";
+    import "quill/dist/quill.core.css";
+    import "quill/dist/quill.snow.css";
+    import "quill/dist/quill.bubble.css";
 
-export default {
-  components: {
-    quillEditor
-  },
-  data() {
-    return {
-      userInfo: JSON.parse(sessionStorage.getItem("userInfo")),
-      activeName: "jinju",
-      ruleForm: {
-        type: "",
-        content: ""
-      },
-      rules: {
-        type: [
-          { required: true, message: "请选择金句类型", trigger: "change" }
-        ],
-        content: [
-          { required: true, message: "请填写金句内容", trigger: "blur" }
-        ]
-      },
+    // 工具栏配置
+    const toolbarOptions = [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['blockquote', 'code-block'],
 
-      articleRules: {
-        title: [{ required: true, message: "请输入文章标题" }],
-        summary: [{ required: true, message: "请输入文章概述" }],
-        type: [{ required: true, message: "请选择文章类型" }],
-        imageUrl: [{ required: true, message: "请选择上传文章封面" }]
-      },
+        [{'header': 1}, {'header': 2}],               // custom button values
+        [{'list': 'ordered'}, {'list': 'bullet'}],
+        [{'script': 'sub'}, {'script': 'super'}],      // superscript/subscript
+        [{'indent': '-1'}, {'indent': '+1'}],          // outdent/indent
+        [{'direction': 'rtl'}],                         // text direction
 
-      article: {
-        content: "<h2>I am Example</h2>",
-        coverImgUrl: "",
-        imageUrl: "",
-      },
-      options: {},
-      postData: {
-        token:
-          "aI9GR6VbK_5gu3kwDj-eTFny-1Hi4sucXf5mQkeg:28x3c--LOhWr6E_R2qP9k_njr9I=:eyJzY29wZSI6Imppbmp1bWFvIiwiZGVhZGxpbmUiOjE1MTU4NTc3NTJ9"
-      }
+        [{'size': ['small', false, 'large', 'huge']}],  // custom dropdown
+        [{'header': [1, 2, 3, 4, 5, 6, false]}],
+
+        [{'color': []}, {'background': []}],          // dropdown with defaults from theme
+        [{'font': []}],
+        [{'align': []}],
+
+        ['clean'],                                       // remove formatting button
+        ['link', 'image', 'video'],
+    ]
+
+    export default {
+        components: {
+            quillEditor
+        },
+        data() {
+            return {
+                userInfo: JSON.parse(sessionStorage.getItem("userInfo")),
+                activeName: "jinju",
+                ruleForm: {
+                    type: "",
+                    content: ""
+                },
+                rules: {
+                    type: [
+                        {required: true, message: "请选择金句类型", trigger: "change"}
+                    ],
+                    content: [
+                        {required: true, message: "请填写金句内容", trigger: "blur"}
+                    ]
+                },
+
+                articleRules: {
+                    title: [{required: true, message: "请输入文章标题"}],
+                    summary: [{required: true, message: "请输入文章概述"}],
+                    type: [{required: true, message: "请选择文章类型"}],
+                    imageUrl: [{required: true, message: "请选择上传文章封面"}]
+                },
+
+                article: {
+                    content: "<h2>I am Example</h2>",
+                    coverImgUrl: "",
+                    imageUrl: "",
+                },
+                options: {  //富文本选项配置
+                    placeholder: '',
+                    theme: 'snow',  // or 'bubble'
+                    modules: {
+                        toolbar: {
+                            container: toolbarOptions,  // 工具栏
+                            handlers: {
+                                'image': function (value) {
+                                    if (value) {
+                                        // 触发input框选择图片文件
+                                        document.querySelector('.quill-uploader input').click()
+                                    } else {
+                                        this.quill.format('image', false);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                postData: {
+                    token:
+                        "aI9GR6VbK_5gu3kwDj-eTFny-1Hi4sucXf5mQkeg:28x3c--LOhWr6E_R2qP9k_njr9I=:eyJzY29wZSI6Imppbmp1bWFvIiwiZGVhZGxpbmUiOjE1MTU4NTc3NTJ9"
+                }
+            };
+        },
+        mounted() {
+            if (!this.userInfo) {
+                this.$message.error("亲，先登陆吧～");
+                return;
+            }
+            this.getUploadToken();
+        },
+        methods: {
+            handleClick(tab) {
+                if (tab.name === "jinju") {
+                }
+            },
+
+            //获取upload token
+            getUploadToken() {
+                QiniuInterface.getUploadToken("2")
+                    .then(data => {
+                        this.postData.token = data;
+                    })
+                    .catch(reason => {
+                        this.$message.error(reason);
+                    });
+            },
+
+            //立即创建
+            submitForm(formName) {
+                if (!this.userInfo) {
+                    this.$message.error("亲，先登陆吧～");
+                    return;
+                }
+                this.$refs[formName].validate(valid => {
+                    if (valid) {
+                        let params = {
+                            user_id: this.userInfo.user_id,
+                            type: this.ruleForm.type,
+                            content: this.ruleForm.content
+                        };
+                        JinjuInterface.createJinju(params)
+                            .then(data => {
+                                this.$message.success("发表成功，还要创建一条吗，加鸡腿哦～");
+                                this.resetForm("ruleForm");
+                            })
+                            .catch(reason => {
+                                this.$message.error(reason);
+                            });
+                    } else {
+                        return false;
+                    }
+                });
+            },
+
+            //重置
+            resetForm(formName) {
+                this.$refs[formName].resetFields();
+            },
+
+            //头像上传成功回调
+            handleAvatarSuccess(res, file) {
+                this.article.imageUrl = URL.createObjectURL(file.raw);
+                this.article.coverImgUrl = res.key;
+            },
+
+            beforeAvatarUpload(file) {
+                const isJPG =
+                    file.type === "image/jpeg" ||
+                    file.type === "image/png" ||
+                    file.type === "image/gif";
+                const isLt2M = file.size / 1024 / 1024 < 2;
+                if (!isJPG) {
+                    this.$message.error("封面只能上传图片!");
+                }
+                if (!isLt2M) {
+                    this.$message.error("封面图片大小不能超过 2MB!");
+                }
+                return isJPG && isLt2M;
+            },
+
+            //富文本中图片上传
+            uploadSuccess(res, file) {
+                let imgUrl = 'http://p2wuhp1bg.bkt.clouddn.com/' + res.key;
+                // res为图片服务器返回的数据
+                // 获取富文本组件实例
+                let quill = this.$refs.myQuillEditor.quill;
+                // 获取光标所在位置
+                let length = quill.getSelection().index;
+                // 插入图片  res.info为服务器返回的图片地址
+                quill.insertEmbed(length, 'image', imgUrl);
+                // 调整光标到最后
+                quill.setSelection(length + 1);
+            },
+
+            //创建文章
+            submitArticle(formName) {
+                this.$refs[formName].validate(valid => {
+                    if (valid) {
+                        MeiwenInterface.createMeiwen(this.article)
+                            .then(data => {
+                                this.$message.success("发表成功，还要创建一篇吗，加鸡腿哦～");
+                            })
+                            .catch(reason => {
+                                this.$message.error(reason);
+                            });
+                    } else {
+                        return false;
+                    }
+                });
+            },
+
+            resetArticle() {
+                this.article.content = "";
+            }
+        }
     };
-  },
-  mounted() {
-    if (!this.userInfo) {
-      this.$message.error("亲，先登陆吧～");
-      return;
-    }
-    this.getUploadToken();
-  },
-  methods: {
-    handleClick(tab) {
-      if (tab.name === "jinju") {
-      }
-    },
-
-    //获取upload token
-    getUploadToken() {
-      QiniuInterface.getUploadToken("2")
-        .then(data => {
-          this.postData.token = data;
-        })
-        .catch(reason => {
-          this.$message.error(reason);
-        });
-    },
-
-    //立即创建
-    submitForm(formName) {
-      if (!this.userInfo) {
-        this.$message.error("亲，先登陆吧～");
-        return;
-      }
-      this.$refs[formName].validate(valid => {
-        if (valid) {
-          let params = {
-            user_id: this.userInfo.user_id,
-            type: this.ruleForm.type,
-            content: this.ruleForm.content
-          };
-          JinjuInterface.createJinju(params)
-            .then(data => {
-              this.$message.success("发表成功，还要创建一条吗，加鸡腿哦～");
-              this.resetForm("ruleForm");
-            })
-            .catch(reason => {
-              this.$message.error(reason);
-            });
-        } else {
-          return false;
-        }
-      });
-    },
-
-    //重置
-    resetForm(formName) {
-      this.$refs[formName].resetFields();
-    },
-
-    //头像上传成功回调
-    handleAvatarSuccess(res, file) {
-      this.article.imageUrl = URL.createObjectURL(file.raw);
-      this.article.coverImgUrl = res.key;
-    },
-
-    beforeAvatarUpload(file) {
-      const isJPG =
-        file.type === "image/jpeg" ||
-        file.type === "image/png" ||
-        file.type === "image/gif";
-      const isLt2M = file.size / 1024 / 1024 < 2;
-      if (!isJPG) {
-        this.$message.error("封面只能上传图片!");
-      }
-      if (!isLt2M) {
-        this.$message.error("封面图片大小不能超过 2MB!");
-      }
-      return isJPG && isLt2M;
-    },
-
-    //创建文章
-    submitArticle(formName) {
-      this.$refs[formName].validate(valid => {
-        if (valid) {
-          MeiwenInterface.createMeiwen(this.article)
-            .then(data => {
-              this.$message.success("发表成功，还要创建一篇吗，加鸡腿哦～");
-            })
-            .catch(reason => {
-              this.$message.error(reason);
-            });
-        } else {
-          return false;
-        }
-      });
-    },
-
-    resetArticle() {
-      this.article.content = "";
-    }
-  }
-};
 </script>
 
 <style scoped>
-.public-container {
-  max-width: 1200px;
-  min-height: 750px;
-  margin: 30px auto;
-}
+    .public-container {
+        max-width: 1200px;
+        min-height: 750px;
+        margin: 30px auto;
+    }
 
-.avatar-uploader .el-upload {
-  border: 1px dashed #d9d9d9;
-  border-radius: 6px;
-  cursor: pointer;
-  position: relative;
-  overflow: hidden;
-}
-.avatar-uploader .el-upload:hover {
-  border-color: #409eff;
-}
-.avatar-uploader-icon {
-  font-size: 28px;
-  color: #8c939d;
-  width: 178px;
-  height: 178px;
-  line-height: 178px;
-  text-align: center;
-  border: 1px solid #ddd;
-  border-radius: 4px;
-}
-.avatar {
-  width: 178px;
-  height: 178px;
-  display: block;
-}
+    .avatar-uploader .el-upload {
+        border: 1px dashed #d9d9d9;
+        border-radius: 6px;
+        cursor: pointer;
+        position: relative;
+        overflow: hidden;
+    }
+
+    .avatar-uploader .el-upload:hover {
+        border-color: #409eff;
+    }
+
+    .avatar-uploader-icon {
+        font-size: 28px;
+        color: #8c939d;
+        width: 178px;
+        height: 178px;
+        line-height: 178px;
+        text-align: center;
+        border: 1px solid #ddd;
+        border-radius: 4px;
+    }
+
+    .avatar {
+        width: 178px;
+        height: 178px;
+        display: block;
+    }
 </style>
