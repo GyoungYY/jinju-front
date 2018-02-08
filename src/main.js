901c6a6d8cd101450ce08d47fca91766a1d57174
yangguang <yangguang@bilibili.com>
2018-02-08 11:53:09 +0800

    【性能优化】引入element-ui theme-chalk

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+/*********性能优化**************
+ *【1】引入jquery和bootstrap cdn文件：注释main.js中import，再index.html文件中引入
+ * 【2】引入element-ui/lib/theme-chalk/index.css cdn文件：同上
+ ******************************/
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -7,8 +13,8 @@ import router from './router'
 Vue.config.productionTip = false
 
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-
+// import 'element-ui/lib/theme-chalk/index.css'
+//
 Vue.use(ElementUI)
 
 // import $ from 'jquery'
